293ba10aa424480916c1b0f7061f11f1b33494e9
Arnaud PY <arnaud.py@student-cs.fr>
2025-03-28 15:04:44 +0100

    ResizeFunction Up on Azure

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace HelloWorld;
+
+using System.Diagnostics;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+class Personne
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+
+    public Personne(string Nom, int Age)
+    {
+        this.Nom = Nom;
+        this.Age = Age;
+    }
+
+    public void Hello(bool isLowercase)
+    {
+        if (isLowercase)
+        {
+            Console.WriteLine($"hello {Nom}, you are {Age}");
+        }
+        else
+        {
+            Console.WriteLine($"HELLO {Nom}, YOU ARE {Age}");
+        }
+    }
+}
+
+class Program
+{
+
+    static string chemin_images =
+        @"C:\Users\arnau\OneDrive - CentraleSupelec\Architecture applicative\TP4 - .NET\images\";
+    static string nouveau_chemin_image =
+        @"C:\Users\arnau\OneDrive - CentraleSupelec\Architecture applicative\TP4 - .NET\images\nouvelles\";
+
+
+    static void Main(string[] args)
+    {
+        Personne p = new Personne("Arnaud", 22);
+        string json = JsonConvert.SerializeObject(p, Formatting.Indented);
+        Console.WriteLine(json);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        Parallel.ForEach(Directory.EnumerateFiles(chemin_images), (chemin_image) =>
+            {
+                TraiterImage(chemin_image, nouveau_chemin_image + Path.GetFileName(chemin_image));
+            }
+        );
+
+        stopwatch.Stop();
+        Console.WriteLine($"Temps d'exécution parallélisé : {stopwatch.ElapsedMilliseconds} ms");
+
+        Stopwatch stopwatch2 = Stopwatch.StartNew();
+
+        foreach (string chemin_image in Directory.EnumerateFiles(chemin_images))
+        {
+            TraiterImage(chemin_image, nouveau_chemin_image + Path.GetFileName(chemin_image));
+        }
+
+        stopwatch2.Stop();
+        Console.WriteLine($"Temps d'exécution sérialisé : {stopwatch2.ElapsedMilliseconds} ms");
+    }
+
+    public static void TraiterImage(string chemin_image, string nouveau_chemin_image)
+    {
+        using (Image image = Image.Load(chemin_image))
+        {
+            image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2).Grayscale());
+            image.Save(nouveau_chemin_image);
+        }
+    }
+}
---
+++ ResizeFunction/ResizeHttpTrigger.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace PyArnaud.FunctionApp
+{
+    public static class ResizeHttpTrigger
+    {
+        [FunctionName("ResizeHttpTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (!int.TryParse(req.Query["w"], out int w) || !int.TryParse(req.Query["h"], out int h))
+            {
+                return new BadRequestObjectResult("Les paramètres 'w' et 'h' doivent être des entiers valides.");
+            }
+
+            byte[]  targetImageBytes;
+            using(var  msInput = new MemoryStream())
+            {
+                // Récupère le corps du message en mémoire
+                await req.Body.CopyToAsync(msInput);
+                msInput.Position = 0;
+
+                // Charge l'image       
+                using (var image = Image.Load(msInput)) 
+                {
+                    // Effectue la transformation
+                    image.Mutate(x => x.Resize(w, h));
+
+                    // Sauvegarde en mémoire               
+                    using (var msOutput = new MemoryStream())
+                    {
+                        image.SaveAsJpeg(msOutput);
+                        targetImageBytes = msOutput.ToArray();
+                    }
+                }
+            }
+
+            return new FileContentResult(targetImageBytes, "image/jpeg");
+
+        }
+    }
+}
